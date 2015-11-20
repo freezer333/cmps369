754509c909305d50c626f407c7a85accd45a4d6e
Scott Frees <scott.frees@gmail.com>
2015-11-20 09:57:30 -0500

    guessing game with express

--- code/16_guessing_express/guess/app.js
+++ code/16_guessing_express/guess/app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ex_session = require('express-session');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(ex_session( { secret : 'cmps369'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
